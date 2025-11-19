025ad7d457d474954444625756dd6eb56a98581e
Misa Valencia <misavalmad@gmail.com>
2025-11-19 14:30:57 -0600

    fix: console.log mas detallado

--- script.js
+++ script.js
@@ -44,8 +44,8 @@ console.log('Suma de strings prueba:', pruebasuma(num1, num2));
 
 //Prueba 3 Usando parseFloat para sumar strings
 
-console.log('Suma de strings con function sumar:', sumar('5', '5'))
-console.log('Resta de strings:', restar('5','3'))
-console.log('Multiplicacion de strings:', multiplicar('8', '8'));
-console.log('Division de strings:', dividir('15', '3'));
-console.log('Division entre 0 con strings:', dividir('5', '0'))
+console.log('Suma de strings con function sumar 5 + 5 =', sumar('5', '5'))
+console.log('Resta de strings 5 - 3 =', restar('5','3'))
+console.log('Multiplicacion de strings 8 x 8 =', multiplicar('8', '8'));
+console.log('Division de strings 15 / 3 =', dividir('15', '3'));
+console.log('Division entre 0 con strings 5 / 0 =', dividir('5', '0'))
