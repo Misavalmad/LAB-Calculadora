d3b7be6bfa72136a9fa271852714debb933780e6
Misa Valencia <misavalmad@gmail.com>
2025-11-19 13:28:24 -0600

    feat:if agregado a funcion dividir para evitar dividir entre cero

--- script.js
+++ script.js
@@ -11,6 +11,9 @@ function multiplicar(a, b){
 }
 
 function dividir(a, b){
+    if (b === 0){
+        return 'No se puede dividir entre 0';
+    }
     return a / b;
 }
 
@@ -20,3 +23,4 @@ console.log('Suma 5 + 5 =', sumar(5, 5));
 console.log('Resta 8 - 4 =', restar(8, 4));
 console.log('Multiplicacion 8 x 8 =', multiplicar(8, 8));
 console.log('Division 15 / 3 =', dividir(15, 3));
+console.log('Division entre 0, 5 / 0', dividir(5, 0))
