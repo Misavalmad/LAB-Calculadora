771982c4d83981586bc73432a844d824e2abc77d
Misa Valencia <misavalmad@gmail.com>
2025-11-19 13:38:37 -0600

    feat: probando suma de strings con console.log

--- script.js
+++ script.js
@@ -17,10 +17,19 @@ function dividir(a, b){
     return a / b;
 }
 
-//Pruebas
+//Prueba 1
 
 console.log('Suma 5 + 5 =', sumar(5, 5));
 console.log('Resta 8 - 4 =', restar(8, 4));
 console.log('Multiplicacion 8 x 8 =', multiplicar(8, 8));
 console.log('Division 15 / 3 =', dividir(15, 3));
 console.log('Division entre 0, 5 / 0', dividir(5, 0))
+
+//Prueba 2 Sumando strings
+
+let num1 = '10';
+let num2 = '20';
+function pruebasuma(num1, num2){
+    return num1 + num2
+}
+console.log('Suma de strings prueba:', pruebasuma(num1, num2));
