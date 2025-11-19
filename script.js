36854b20eaebf39fc16a7487136fabe8d7c61f61
Misa Valencia <misavalmad@gmail.com>
2025-11-19 13:23:34 -0600

    feat:logica de script agregada junto con pruebas

---
+++ script.js
@@ -0,0 +1,22 @@
+function sumar(a, b){
+    return a + b;
+}
+
+function restar(a, b){
+    return a - b;
+}
+
+function multiplicar(a, b){
+    return a * b;
+}
+
+function dividir(a, b){
+    return a / b;
+}
+
+//Pruebas
+
+console.log('Suma 5 + 5 =', sumar(5, 5));
+console.log('Resta 8 - 4 =', restar(8, 4));
+console.log('Multiplicacion 8 x 8 =', multiplicar(8, 8));
+console.log('Division 15 / 3 =', dividir(15, 3));
